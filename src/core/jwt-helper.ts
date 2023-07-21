7fb1eea1fe057556ce5ee3bb638347454294edc2
jackychoi26 <gmu1233@gmail.com>
2023-07-21 21:51:08 +0800

    feat: updated the token expiration time

--- src/core/jwt-helper.ts
+++ src/core/jwt-helper.ts
@@ -3,9 +3,12 @@ import UnauthorizedError from './errors/unauthorized-error';
 
 export default class JwtHelper {
   // expires in 3 months
-  static sign(data: TokenData, expiresIn: number = 60 * 60 * 24 * 90): string {
+  static sign(
+    data: TokenData,
+    expiresIn: number = 1000 * 60 * 60 * 24 * 90
+  ): string {
     return jwt.sign(data, process.env.JWT_SECRET!, {
-      expiresIn: 1000
+      expiresIn: expiresIn
     });
   }
 
